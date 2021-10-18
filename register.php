a694c11017e1a1318bb9fa80ae7cd22ff5b398a2
pencilpram <pencilpram@gmail.com>
2021-10-18 23:06:38 +0700

    new commit

--- register.php
+++ register.php
@@ -50,15 +50,17 @@ if ($objResult) {
 
     // GET LAST UNIQUE ID
     $query = "SELECT MAX(TRIM(LEADING 'U' FROM `userid`)) as user_id FROM `users`;";
-    $result = $db->query($query) or die('There was an error running the query [' . $db->error . ']');
+    $result = $mysqli->query($query) or die('There was an error running the query [' . $mysqli->error . ']');
     // ADD +1 WITH LAST UNIQUE ID
     $row = $result->fetch_assoc();
     $last_user_id = empty($row['user_id']) ? 00 : $row['user_id'];
-    $next_user_id = 'U' . ($last_emp_id + 1);
+    $next_user_id = 'U' . ($last_user_id + 1);
 
-    $strSQL = "INSERT INTO users (username,password,firstname,lastname,usergroup,title,Gender,email,birthdate) VALUES ('" . $_POST["txtUsername"] . "', 
-		'" . $_POST["txtPassword"] . "','" . $_POST["txtFirstname"] . "','" . $_POST["txtLastname"] . "','" . $_POST["optusergroup"] . "','" . $_POST["opttitle"] . "',
-        '" . $_POST["optGender"] . "','" . $_POST["txtEmail"] . "','" . $_POST["birthdate"] . "')";
+    $strSQL = "INSERT INTO users (username,password,firstname,lastname,usergroup,title,Gender,email,birthdate) 
+    VALUES ('" . $_POST["txtUsername"] . "','" . $_POST["txtPassword"] . "','" . $_POST["txtFirstname"] . "',
+    '" . $_POST["txtLastname"] . "','" . $_POST["optusergroup"] . "','" . $_POST["opttitle"] . "',
+    '" . $_POST["optGender"] . "','" . $_POST["txtEmail"] . "','" . $_POST["birthdate"] . "')";
+    
     $objQuery = mysqli_query($mysqli, $strSQL);
 
     echo "Register Completed!<br>";
