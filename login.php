d4788e6d012645a1a0322bd282a9b2a58d4915c8
pencilpram <pencilpram@gmail.com>
2021-10-18 23:34:13 +0700

    new commit

--- login.php
+++ login.php
@@ -8,43 +8,44 @@ if ($mysqli->connect_errno) {
     echo $mysqli->connect_error;
 }
 
-// if (isset($_POST['username'])) {
-//     //รับค่า user & password
-//     $username = $_POST['username'];
-//     $password = ($_POST['password']);
+if (isset($_POST['username'])) {
+    //รับค่า user & password
+    $username = $_POST['username'];
+    $password = ($_POST['password']);
 
-//     //query 
-//     $sql = "SELECT * FROM users Where username='" . [$username] . "' and password='" . [$password] . "' ";
+    //query 
+    $sql = "SELECT * FROM users Where username='" . [$username] . "' and password='" . [$password] . "' ";
 
-//     $result = mysqli_query($mysqli, $sql);
+    $result = mysqli_query($mysqli, $sql);
 
-//     // หาจำนวนเรกคอร์ดข้อมูล
-//     if (mysqli_num_rows($result) == 1) {
-//         header("location: home-Final.html"); //ไปไปตามหน้าที่คุณต้องการ
-//     } else {
-//         $code_error = "<BR><FONT COLOR=\"red\">Incorrect Username or Password</FONT>";
-//         echo ($code_error);
-//         header("location: Final-Login.html"); //ไม่ถูกต้องให้กับไปหน้าเดิม
-//     }
-// }
-
-$strSQL = "SELECT * FROM users WHERE username = '" . mysqli_real_escape_string($mysqli,$_POST['username']) . "' 
-	and password = '" . mysqli_real_escape_string($mysqli,$_POST['password']) . "'";
-$objQuery = mysqli_query($mysqli,$strSQL);
-$objResult = mysqli_fetch_array($objQuery);
-if (!$objResult) {
-    echo "Username and Password Incorrect!";
-} else {
-    // $_SESSION["UserID"] = $objResult["UserID"];
-    // $_SESSION["Status"] = $objResult["Status"];
-
-    session_write_close();
-
-    if ($objResult) {
-        header("location:home-Final.html");
+    // หาจำนวนเรกคอร์ดข้อมูล
+    if (mysqli_num_rows($result) == 1) {
+        header("location: home-Final.html"); //ไปไปตามหน้าที่คุณต้องการ
     } else {
-        header("location:Final-Login.html");
+        $code_error = "<BR><FONT COLOR=\"red\">Incorrect Username or Password</FONT>";
+        echo ($code_error);
+        header("location: Final-Login.html"); //ไม่ถูกต้องให้กับไปหน้าเดิม
     }
 }
-mysqli_close($mysqli);
 ?>
+
+// $strSQL = "SELECT * FROM users WHERE username = '" . mysqli_real_escape_string($mysqli,$_POST['username']) . "' 
+// 	and password = '" . mysqli_real_escape_string($mysqli,$_POST['password']) . "'";
+// $objQuery = mysqli_query($mysqli,$strSQL);
+// $objResult = mysqli_fetch_array($objQuery);
+// if (!$objResult) {
+//     echo "Username and Password Incorrect!";
+// } else {
+//     // $_SESSION["UserID"] = $objResult["UserID"];
+//     // $_SESSION["Status"] = $objResult["Status"];
+
+//     session_write_close();
+
+//     if ($objResult) {
+//         header("location:home-Final.html");
+//     } else {
+//         header("location:Final-Login.html");
+//     }
+// }
+// mysqli_close($mysqli);
+
