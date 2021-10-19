a4e383c4615435425dc1ad918e650f8b1038162e
pencilpram <pencilpram@gmail.com>
2021-10-19 22:17:37 +0700

    new commit by Pram

--- search.php
+++ search.php
@@ -3,12 +3,14 @@
     $mysqli = new mysqli("localhost", "root", null, "Barami_Library");
 
     if ($mysqli->connect_errno) {
-        echo $mysqli->connect_error;
+        die("Connection failed: " . $mysqli->connect_error);
     }
 
-    if (isset($_POST["querytype"]) == "Select Search" && (isset($_GET["category"]) == "Select Category")) {
-        $query = "SELECT booktitle, authorsname, genre FROM booksinformation WHERE (booktitle LIKE '%$search%')
-        OR (authorsname LIKE '%$search%') OR (genre LIKE '%$search%')";
+    $search = $_GET['query'];
+
+    if (isset($_GET["querytype"]) == "Select Search" && (isset($_GET["category"]) == "Select Category")) {
+        $query = "SELECT booktitle, authorsname, genre FROM booksinformation WHERE (booktitle LIKE '$search')
+        OR (authorsname LIKE '$search') OR (genre LIKE '$search')";
         echo "<tr>";
         echo "<td class='col-2'>$query</td>";
         echo "<td class='col-4'>$query</td>";
