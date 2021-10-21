28cb10624a3214d6390cc910085dabb1e3328319
BobbyAutumn <job.pasin@gmail.com>
2021-10-22 00:45:23 +0700

    commit on 'main'

--- home-Final.php
+++ home-Final.php
@@ -73,7 +73,7 @@
                         <span>Author Name</span>
                     </div>
                     <div class="col-2" style="width:240px; text-align:center">
-                        <span>Category</span>
+                        <span>Publisher</span>
                     </div>
                     <div class="col-1" style="width: 120px;"></div>
                     <div class="col-1" style="width: 120px;"></div>
@@ -178,6 +178,26 @@
                                 echo "</table>";
                             }
                         }
+                    }
+                } elseif ($searchby  == "Select Search" && $category != "Select Category") {
+                    $query = "SELECT * FROM booksinfomation WHERE genre LIKE '%$category%'";
+                    $result = $mysqli->query($query);
+                    if ($result) {
+                        echo "<table class='table'>";
+                        while ($row = $result->fetch_array()) {
+                            echo "<tr>";
+                            echo "<td class='col-4' style='text-align:center;'>" . $row["booktitle"] . "</td>";
+                            echo "<td class='col-4' style='text-align:center;'>" . $row["authorsname"] . "</td>";
+                            echo "<td class='col-2' style='text-align:center;'>" . $row["publisher"] . "</td>";
+                            if ($row["available_amount"] == 0) {
+                                echo "<td class='col-1' style='background-color:red; text-align:center;'>B</td>";
+                            } else {
+                                echo "<td class='col-1' style='background-color:lightgreen; text-align:center;'>B</td>";
+                            }
+                            echo "<td class='col-1' style='text-align:center; margin-right:10px;'><img src='loupe.png' width='24' height='24'></td>";
+                            echo "</tr>";
+                        }
+                        echo "</table>";
                     } else {
                         //echo "5";
                         $query = "SELECT * FROM booksinfomation";
