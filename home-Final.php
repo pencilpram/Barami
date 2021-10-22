927a3dd93230092f507ed9f4ca9de0ae00b0a449
pencilpram <pencilpram@gmail.com>
2021-10-23 00:36:58 +0700

    new commit

--- home-Final.php
+++ home-Final.php
@@ -431,7 +431,7 @@
 
                     </div>
                     <div class="col-2" style="width: 15%; margin-top: 25px;">
-                        <form role="form" method="post" action="logout.php" style="height: 50px; display: flex; 
+                        <form role="form" method="post" action="home-Final.php" style="height: 50px; display: flex; 
                                             justify-content: center; align-content: center; flex-direction: column;">
                             <input type="submit" class="col btn btn-primary" style="height: 50px; 
                                                 border-top-left-radius: 35px; border-top-right-radius: 35px; border-bottom-left-radius: 35px; 
