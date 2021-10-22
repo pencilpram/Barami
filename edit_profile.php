e58e02609cc341f515b7d339049e9febfb6ec98e
pencilpram <pencilpram@gmail.com>
2021-10-23 00:46:04 +0700

    new commit

--- edit_profile.php
+++ edit_profile.php
@@ -3,7 +3,8 @@ $mysqli = new mysqli("localhost", "root", null, "Barami_Library");
 
 
 //Update Database
-$update_sql = "UPDATE users SET title='$title', firstname='$firstname', lastname='$lastname', age='$age', birthdate='$birthdate', gender='$gender', usergroup='$usergroup', email='$email' WHERE userid='$userid'";
+$update_sql = "UPDATE users SET title='$_POST[title]', firstname='$_POST[firstname]', lastname='$_POST[lastname]', 
+age='$_POST[age]', birthdate='$_POST[birthdate]', gender='$_POST[gender]', usergroup='$_POST[usergroup]', 
+email='$_POST[email]' WHERE userid='$userid'";
 $update_result = $mysqli->query($update_sql);
 header("location: home-Final.php"); //ไปตามหน้าที่คุณต้องการ
-?>
