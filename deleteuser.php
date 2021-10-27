6e2f118cb6859eb0a11217fc5ae820cd5be8c351
pencilpram <pencilpram@gmail.com>
2021-10-27 23:39:46 +0700

    new commit

--- deleteuser.php
+++ deleteuser.php
@@ -3,7 +3,7 @@ $mysqli = new mysqli("localhost", "root", null, "Barami_Library");
 if (isset($_GET["userid"])) {
     $userid = $_GET["userid"];
     $delete_query = "DELETE FROM users WHERE userid = '$userid'";
-    $delete_result = $mysqli->query($delete_result);
+    $delete_result = $mysqli->query($delete_query);
     if (!$delete_result) {
         echo "Delete failed!<br/>";
         echo $mysqli->error;
