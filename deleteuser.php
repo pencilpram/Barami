aba9aa437f9d3d184d2b1b32a37423d82307f25c
pencilpram <pencilpram@gmail.com>
2021-10-27 23:30:37 +0700

    new commit

---
+++ deleteuser.php
@@ -0,0 +1,14 @@
+<?php
+$mysqli = new mysqli("localhost", "root", null, "Barami_Library");
+if (isset($_GET["userid"])) {
+    $userid = $_GET["userid"];
+    $delete_query = "DELETE FROM users WHERE userid = '$userid'";
+    $delete_result = $mysqli->query($delete_result);
+    if (!$result) {
+        echo "Delete failed!<br/>";
+        echo $mysqli->error;
+    } else {
+        header("location: home-Final-Adminmode.php");
+    }
+}
+?>
