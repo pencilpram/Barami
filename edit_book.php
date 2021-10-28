777e15ffd18b9689715be5a036515182557ddae7
pencilpram <pencilpram@gmail.com>
2021-10-28 16:20:42 +0700

    new commit

---
+++ edit_book.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$mysqli = new mysqli("localhost", "root", null, "Barami_Library");
+
+$booktitle = $_POST['booktitle'];
+$authorsname = $_POST['authorsname'];
+$publisher = $_POST['publisher'];
+$numberofpage = $_POST['numberofpage'];
+$genre = $_POST['genre'];
+$amount = $_POST['amount'];
+$available_amount = $_POST['available_amount'];
+$booksid=$_SESSION['booksinformationid'];
+
+//Update Database
+$update_sql = "UPDATE booksinformation SET booktitle='$booktitle', authorsname='$authorsname', publisher='$publisher', 
+numberofpage='$numberofpage', genre='$genre', amount='$amount', available_amount='$available_amount' WHERE booksinformationid='$booksid'";
+$update_result = $mysqli->query($update_sql);
+header("location: home-Final-Adminmode.php");
